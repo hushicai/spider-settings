61492b9c9c7c792d24450b505d37aaa985be5147
hushicai <bluthcy@gmail.com>
2017-04-05 20:32:37 +0800

    update

--- index.js
+++ index.js
@@ -3,14 +3,6 @@
  * @author hushicai(bluthcy@gmail.com)
  */
 
-function _get(db) {
-  return {
-    host: 'localhost',
-    port: 6379,
-    db: db
-  }
-}
-
 exports = require('./conf/redis');
 
 exports.webconfig = require('./conf/webconfig');
